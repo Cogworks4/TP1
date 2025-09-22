440dac478e3d2481663eedb4c21ce781bed7b167
Jeffrey Xiao <fragment004c@gmail.com>
2025-09-22 15:53:30 -0700

    changed emailRecognizer back to accepting string
    readapted emailRecognizer back to accepting strings, Optional<String> is now being handled inside viewUserUpdate before passing a String into validateEmail()

--- FoundationsF25/src/emailRecognizer/emailRecognizer.java
+++ FoundationsF25/src/emailRecognizer/emailRecognizer.java
@@ -1,7 +1,5 @@
 package emailRecognizer;
 
-import java.util.Optional;
-
 /* 
  * Description:
  * The email recognizer takes the input of a of a text prompt from a GUI page 
@@ -18,18 +16,19 @@ public class emailRecognizer {
 	static int notAllowedCharSize = notAllowedChar.length;
 
 	static public void main(String[] args) {
-		// testing(1, "jacob..henry@gmail.com");
-		// testing(2, "jacob]henry@gmail.com");
-		// testing(3, "jacob@henry@gmail.com");
-		// testing(4, "jacob.henry@gmail.c");
-		// testing(5, ".jacob.henry@gmail.com");
-		// testing(6, "jacobhenry.@gmail.com");
-		// testing(7, "jacobhenry@-gmail.com");
-		// testing(8, "jacobhenry@gmail-.com");
-		// testing(9, "jacobhenry@gmail.c0m");
-		// testing(10, "jacobhenry");
-		// testing(11, "jacobhenry@.gmail.com");
-		// testing(12, "jacobhenry@gmail.com");
+		testing(1, "jacob..henry@gmail.com");
+		testing(2, "jacob]henry@gmail.com");
+		testing(3, "jacob@henry@gmail.com");
+		testing(4, "jacob.henry@gmail.c");
+		testing(5, ".jacob.henry@gmail.com");
+		testing(6, "jacobhenry.@gmail.com");
+		testing(7, "jacobhenry@-gmail.com");
+		testing(8, "jacobhenry@gmail-.com");
+		testing(9, "jacobhenry@gmail.c0m");
+		testing(10, "jacobhenry");
+		testing(11, "jacobhenry@.gmail.com");
+		testing(12, "jacobhenry@gmail.com");
+		testing(13, "test@gmail.com.");
 	}
 
 	/*
@@ -45,9 +44,8 @@ public class emailRecognizer {
 	 * @param result
 	 * @return
 	 */
-	static public String validateEmail(Optional<String> result) {
-		if (result.isPresent()) {
-			inputIndex = result.get().length(); // updates the length of the input index
+	static public String validateEmail(String result) {
+			inputIndex = result.length(); // updates the length of the input index
 			int symbolCount = 0; // the count for a specified symbol
 			int atSymbol = 0; // remembers if the email has a domain
 
@@ -64,7 +62,7 @@ public class emailRecognizer {
 					if (result.get().charAt(i) == notAllowedChar[j]) {
 
 						// If it's only one @ symbol continue
-						if (result.get().charAt(i) == '@') {
+						if (result.charAt(i) == '@') {
 							symbolCount += 1;
 							// If there are are more than one @ symbol return the appropriate error
 							if (symbolCount > 1) {
@@ -72,11 +70,11 @@ public class emailRecognizer {
 							}
 						}
 
-						if (result.get().charAt(i) != '@') {
+						if (result.charAt(i) != '@') {
 							// Returns a appropriate error
 							errorMessage = "You cannont use the ' ' character";
 							StringBuilder sb = new StringBuilder(errorMessage);
-							sb.setCharAt(21, result.get().charAt(i));
+							sb.setCharAt(21, result.charAt(i));
 							return errorMessage = sb.toString();
 						}
 					}
@@ -84,11 +82,11 @@ public class emailRecognizer {
 					// Checks for any dots at the first index, last index before the @ or after the
 					// @
 					// symbol, or any subsequent dots
-					if (result.get().charAt(i) == '.' && i == 0) { // dot at start
+					if (result.charAt(i) == '.' && i == 0) { // dot at start
 						return errorMessage = "You cannont use the '.' char at the beginning of your email";
-					} else if (result.get().charAt(i) == '.' && result.get().charAt(i + 1) == '@') { // dot before @
+					} else if (result.charAt(i) == '.' && result.charAt(i + 1) == '@') { // dot before @
 						return errorMessage = "You cannont use the '.' char right after the @ symbol";
-					} else if (result.get().charAt(i) == '.' && result.get().charAt(i + 1) == '.') { // consecutive dots
+					} else if (result.charAt(i) == '.' && result.charAt(i + 1) == '.') { // consecutive dots
 						return errorMessage = "You cannont use the '.' char consecutively";
 					}
 				}
@@ -98,18 +96,18 @@ public class emailRecognizer {
 
 			// Checks for extra dots and a minimum char limit for the TLD
 			for (int i = 0; i < inputIndex; i++) {
-				if (result.get().charAt(i) == '@') {
+				if (result.charAt(i) == '@') {
 
 					// Remembers the @ symbol was in the input
 					atSymbol = 1;
 
 					// you cannot start with a hyphen after the @
-					if (result.get().charAt(i + 1) == '-') {
+					if (result.charAt(i + 1) == '-') {
 						return errorMessage = "You cannont use a hyphen after the @ symbol";
 					}
 
 					for (int j = i; j < inputIndex; j++) {
-						if (result.get().charAt(j) == '.') {
+						if (result.charAt(j) == '.') {
 
 							// Checks the minimum of two characters for the TLD
 							if (j + 3 > inputIndex) {
@@ -117,13 +115,13 @@ public class emailRecognizer {
 							}
 
 							// you cannot end with a hyphen after the @ before the TLD
-							if (result.get().charAt(j - 1) == '-') {
+							if (result.charAt(j - 1) == '-') {
 								return errorMessage = "You cannont use a hyphen right before the TLD (.com, .net, etc)";
 							}
 
 							// Checks for only alphabetic characters for the TLD
 							for (int k = j + 1; k < inputIndex; k++) {
-								if (!Character.isLetter(result.get().charAt(k))) {
+								if (!Character.isLetter(result.charAt(k))) {
 									return errorMessage = "You must have a valid TLD (.com, .net, etc)";
 								}
 							}
@@ -138,11 +136,9 @@ public class emailRecognizer {
 			}
 
 			return "";
-		} else
-			return "";
 	}
 
-	static public void testing(int number, Optional<String> Email) {
+	static public void testing(int number, String Email) {
 		System.out.print(number + ".\n" + Email + "\nError Message:   ");
 		System.out.println(validateEmail(Email) + "\n\n");
 	}
