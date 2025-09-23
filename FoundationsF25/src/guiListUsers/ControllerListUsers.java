e32b8f396e0b6bea2fbe753c915c3916a8107d8c
Jacob Sheridan <Jacob.Henry.Sheridan@gmail.com>
2025-09-23 00:52:04 -0700

    Updated guiListUsers package
    Took outline from the guiAddRemoveRoles package, got a basic display with a title, user, return button, and divider lines.

--- FoundationsF25/src/guiListUsers/ControllerListUsers.java
+++ FoundationsF25/src/guiListUsers/ControllerListUsers.java
@@ -1,5 +1,17 @@
 package guiListUsers;
 
+/*******
+ * <p> Title: ControllerlListUsers Class. </p>
+ * 
+ * <p> Description: </p>
+ * 
+ * @author Jacob Sheridan
+ * 
+ * @version 1.00		2025-09-22 Initial version
+ *  
+ */
+
+
 import database.Database;
 import guiAddRemoveRoles.ViewAddRemoveRoles;
 import javafx.collections.FXCollections;
@@ -13,18 +25,17 @@ public class ControllerListUsers {
 		private static Database theDatabase = applicationMain.FoundationsMain.database;	
 		
 		protected static void repaintTheWindow() {
-			ViewListUsers.theRootPane.getChildren().addAll(
-					ViewListUsers.label_PageTitle, ViewListUsers.label_UserDetails, 
-					ViewListUsers.button_UpdateThisUser, ViewListUsers.line_Separator1,
-					ViewListUsers.label_SelectUser, ViewListUsers.combobox_SelectUser, 
-					ViewListUsers.line_Separator4, ViewListUsers.button_Return,
-					ViewListUsers.button_Logout, ViewListUsers.button_Quit);
+			ViewListUsers.theRootPane.getChildren().setAll(
+					ViewListUsers.label_PageTitle, ViewListUsers.label_UserDetails,
+					ViewListUsers.line_Separator1,
+					ViewListUsers.line_Separator4, 
+					ViewListUsers.button_Return);
 			
 			// Add the list of widgets to the stage and show it
 			
 			// Set the title for the window
-			ViewListUsers.theStage.setTitle("CSE 360 Foundation Code: Admin Opertaions Page");
-			ViewListUsers.theStage.setScene(ViewListUsers.theAddRemoveRolesScene);
+			ViewListUsers.theStage.setTitle("CSE 360 Foundation Code: List Users Page");
+			ViewListUsers.theStage.setScene(ViewListUsers.theListUsersScene);
 			ViewListUsers.theStage.show();
 		}
 		
--- FoundationsF25/src/guiListUsers/ModelListUsers.java
+++ FoundationsF25/src/guiListUsers/ModelListUsers.java
@@ -1,5 +1,16 @@
 package guiListUsers;
 
+/*******
+ * <p> Title: ModelListUsers Class. </p>
+ * 
+ * <p> Description: The ListUsers Page Model.  This class is not used as there is no
+ * data manipulated by this MVC.</p>
+ * 
+ * @author Jacob Sheridan
+ * 
+ * @version 1.00		2025-09-22 Initial version
+ *  
+ */
 
 public class ModelListUsers {
 }
